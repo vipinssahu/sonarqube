6a161087575286c388155381f8b4281cc861dce8
vipsahu <vipsahu@LIN86000019.corp.capgemini.com>
2020-05-20 18:08:40 +0530

    test

---
+++ src/main/java/com/test/sonarqube/App.java
@@ -0,0 +1,14 @@
+package com.test.sonarqube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a;
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/test/sonarqube/Test.java
@@ -0,0 +1,5 @@
+package com.test.sonarqube;
+
+public class Test {
+
+}
